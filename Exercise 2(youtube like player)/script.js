d6c1cf424dfc63b49570cce3b60669d5188da131
Kanhaiya Kumar Sahu <2021mcb1235@iitrpr.ac.in>
2025-10-14 04:15:21 +0530

    made dedicated folder for all my practice projects

---
+++ Exercise 2(youtube like player)/script.js
@@ -0,0 +1,48 @@
+let videos = [ "4879375_Gyps Fulvus_Griffon Vulture_3840x2160" , "456540_Thailand_Bangkok_1920x1080" , "6356499_Herd_Afghanistan_3840x2160" ,"big_buck_bunny_720p_10mb"]
+        let last_video_in_the_view = 0;
+        let videoList = document.querySelector(".video_playlist")
+        videos.forEach((name,i)=>{
+            videoList.insertAdjacentHTML(
+                "beforeend",
+                `<div class="video_in_list video${i}" data-key="${i}">
+                <video data-key="${i}" width="157px" >
+                    <source src="./assets/${videos[i]}.mp4">
+                </video>
+                <div class="description" data-key="${i}">
+                    <h5 data-key="${i}">description${i+1}: Lorem ipsum dolor, sit amet consectetur adipisicing.</h5>
+                    <p data-key="${i}">Channel ${i+1}</p>
+                </div>
+            </div>`
+            )
+        })
+        let tagList = [];
+        for(let i = 0;i<4;i++)
+        {
+            tagList.push(document.querySelector(`.video${i}`));
+        }
+        let playBox = document.querySelector(".play_box");
+        playBox.innerHTML = `<video controls autoplay muted loop width="100%" >
+                <source src="./assets/4879375_Gyps Fulvus_Griffon Vulture_3840x2160.mp4">
+            </video>
+            <h3>description1 Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+            <p> Channel 1</p>`
+        
+        let playedVideoFromPlaylist = tagList[0];
+        playedVideoFromPlaylist.classList.toggle("toggle")
+        videoList.addEventListener("click",(e)=>{
+            let id = e.target.dataset.key;
+            if(id === undefined || id === last_video_in_the_view){
+                return;
+            }
+            let lastVideo = tagList[last_video_in_the_view];
+            lastVideo.classList.toggle('toggle');
+            playBox.innerHTML = `<video controls autoplay muted loop width="100%" >
+                <source src="./assets/${videos[id]}.mp4">
+            </video>
+            <h3>description${Number(id)+1} Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
+            <p> Channel ${Number(id)+1}</p>`
+            let currVideo = tagList[id];
+            currVideo.classList.toggle(":hover")
+            currVideo.classList.toggle('toggle');
+            last_video_in_the_view = id;
+        })

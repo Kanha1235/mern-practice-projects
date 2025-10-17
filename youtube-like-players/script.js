aaf501786f9ad4a5160ac1764d78e2931895149a
Kanhaiya Kumar Sahu <2021mcb1235@iitrpr.ac.in>
2025-10-17 19:58:37 +0530

    added big video files in release and used those links

--- youtube-like-players/script.js
+++ youtube-like-players/script.js
@@ -1,4 +1,4 @@
-let videos = [ "4879375_Gyps Fulvus_Griffon Vulture_3840x2160" , "456540_Thailand_Bangkok_1920x1080" , "6356499_Herd_Afghanistan_3840x2160" ,"big_buck_bunny_720p_10mb"]
+let videos = [ "https://github.com/Kanha1235/mern-practice-projects/releases/download/v1-assets/456540_Thailand_Bangkok_1920x1080.mp4" , "https://github.com/Kanha1235/mern-practice-projects/releases/download/v1-assets/4879375_Gyps.Fulvus_Griffon.Vulture_3840x2160.mp4" , "https://github.com/Kanha1235/mern-practice-projects/releases/download/v1-assets/6356499_Herd_Afghanistan_3840x2160.mp4" ,"https://github.com/Kanha1235/mern-practice-projects/releases/download/v1-assets/big_buck_bunny_720p_10mb.mp4"]
         let last_video_in_the_view = 0;
         let videoList = document.querySelector(".video_playlist")
         videos.forEach((name,i)=>{
@@ -6,7 +6,7 @@ let videos = [ "4879375_Gyps Fulvus_Griffon Vulture_3840x2160" , "456540_Thailan
                 "beforeend",
                 `<div class="video_in_list video${i}" data-key="${i}">
                 <video data-key="${i}" width="157px" >
-                    <source src="./assets/${videos[i]}.mp4">
+                    <source src="${videos[i]}">
                 </video>
                 <div class="description" data-key="${i}">
                     <h5 data-key="${i}">description${i+1}: Lorem ipsum dolor, sit amet consectetur adipisicing.</h5>
@@ -22,7 +22,7 @@ let videos = [ "4879375_Gyps Fulvus_Griffon Vulture_3840x2160" , "456540_Thailan
         }
         let playBox = document.querySelector(".play_box");
         playBox.innerHTML = `<video controls autoplay muted loop width="100%" >
-                <source src="./assets/4879375_Gyps Fulvus_Griffon Vulture_3840x2160.mp4">
+                <source src="${videos[0]}">
             </video>
             <h3>description1 Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
             <p> Channel 1</p>`
@@ -37,7 +37,7 @@ let videos = [ "4879375_Gyps Fulvus_Griffon Vulture_3840x2160" , "456540_Thailan
             let lastVideo = tagList[last_video_in_the_view];
             lastVideo.classList.toggle('toggle');
             playBox.innerHTML = `<video controls autoplay muted loop width="100%" >
-                <source src="./assets/${videos[id]}.mp4">
+                <source src="${videos[id]}">
             </video>
             <h3>description${Number(id)+1} Lorem ipsum dolor sit amet consectetur adipisicing.</h3>
             <p> Channel ${Number(id)+1}</p>`
